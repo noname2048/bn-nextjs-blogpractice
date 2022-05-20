89265f7ff7e846dd041f41a16fbc85f2e8e8a692
noname2048 <sungwook.csw@gmail.com>
2022-05-20 23:04:08 +0900

    feat: add custom p

--- components/Posts/PostDetail/PostContent.js
+++ components/Posts/PostDetail/PostContent.js
@@ -9,13 +9,43 @@ export default function PostContent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customComponents = {
-    img(image) {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
       return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
         />
       );
     },
