1bf0f01866527329b796d2d0fd8bc3cd45434eb1
noname2048 <sungwook.csw@gmail.com>
2022-05-21 00:21:15 +0900

    feat: add custom code block comp

--- components/Posts/PostDetail/PostContent.js
+++ components/Posts/PostDetail/PostContent.js
@@ -2,6 +2,8 @@ import styles from "./PostContent.module.scss";
 import PostHeader from "./PostHeader";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function PostContent(props) {
   const { post } = props;
@@ -9,44 +11,42 @@ export default function PostContent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customComponents = {
-    // img(image) {
-    //   return (
-    //     <Image
-    //       src={`/images/posts/${post.slug}/${image.src}`}
-    //       alt={image.alt}
-    //       width={600}
-    //       height={300}
-    //     />
-    //   );
-    // },
-    p(paragraph) {
-      const { node } = paragraph;
-      if (node.children[0].tagName === "img") {
-        const image = node.children[0];
-
-        return (
-          <div className={styles.image}>
-            <Image
-              src={`/images/posts/${post.slug}/${image.properties.src}`}
-              alt={image.alt}
-              width={600}
-              height={300}
-            />
-          </div>
-        );
-      }
-
-      return <p>{paragraph.children}</p>;
+    img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
     },
+    // p(paragraph) {
+    //   const { node } = paragraph;
+    //   if (node.children[0].tagName === "img") {
+    //     const image = node.children[0];
+
+    //     return (
+    //       <div className={styles.image}>
+    //         <Image
+    //           src={`/images/posts/${post.slug}/${image.properties.src}`}
+    //           alt={image.alt}
+    //           width={600}
+    //           height={300}
+    //         />
+    //       </div>
+    //     );
+    //   }
+
+    //   return <p>{paragraph.children}</p>;
+    // },
     code(code) {
       const { className, children } = code;
       const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
       return (
-        <SyntaxHighlighter
-          style={atomDark}
-          language={language}
-          children={children}
-        />
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
       );
     },
   };
