6071026cd0d2c423cd6d89ad12ff35b4957fd409
noname2048 <sungwook.csw@gmail.com>
2022-05-24 23:58:28 +0900

    feat: reduce react-markdown code amount

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,8 +2,11 @@ import styles from "./PostContent.module.scss";
 import PostHeader from "./PostHeader";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+
+SyntaxHighlighter.registerLanguage("js", js);
 
 export default function PostContent(props) {
   const { post } = props;
